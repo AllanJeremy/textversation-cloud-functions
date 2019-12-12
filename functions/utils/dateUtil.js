18fba5d05c9f88ee87afd0a02e2720be38d90a8c
AllanJeremy <aj.dev254@gmail.com>
2019-12-12 18:23:42 +0300

    [Bugfix] Get time elapsed works correctly now - The function was not returning any value - Added better docs

--- functions/utils/dateUtil.js
+++ functions/utils/dateUtil.js
@@ -1,8 +1,14 @@
+/** Get the time elapsed in seconds from a firebase timestamp
+ * @param {FirebaseTimestamp} firebaseTimestamp A firebase timestamp
+ * @return {number} Time elapsed since the provided timestamp in seconds
+ */
 function getTimeElapsedTimestampInSeconds(firebaseTimestamp) {
   let timestampInSeconds = firebaseTimestamp.seconds;
   let dateNowInSeconds = Date.now() / 1000;
 
   let timeElapsed = dateNowInSeconds - timestampInSeconds;
+
+  return timeElapsed;
 }
 
 /** Get the date difference in minutes based on the provided firebase timestamp
