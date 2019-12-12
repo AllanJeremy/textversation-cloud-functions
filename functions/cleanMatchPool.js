67f553a294e82e9bdec563068aa20d78b17af847
AllanJeremy <aj.dev254@gmail.com>
2019-12-12 18:41:54 +0300

    [Feature] Clean match pool working - User that has been in match pool for at least 30 minutes is removed

--- functions/cleanMatchPool.js
+++ functions/cleanMatchPool.js
@@ -2,30 +2,28 @@ const db = require("firebase-admin").firestore();
 const dateUtil = require("./utils/dateUtil");
 const poolCollectionRef = db.collection("matchPool");
 
-// Remove user from pool if they have been in pool for this many seconds
-const TIME_TO_REMOVE_SECONDS = 20; /* 30 * 60 */ // 30 minutes
+// Remove user from pool if they have been in pool for this many minutes
+const TIME_TO_REMOVE_MINUTES = 30;
 
 let cleanMatchPool = async () => {
   let poolMembers = await poolCollectionRef.get();
 
-  console.debug("Starting cleanup");
-
-  if (poolMembers.empty) return false;
+  if (!poolMembers.size) return false;
 
   let batch = db.batch();
 
-  console.debug("Cleanup started");
   poolMembers.docs.map(memberSnapshot => {
     let member = memberSnapshot.data();
-    let timeElapsedSeconds = dateUtil.dateDiffSeconds(member.dateJoined);
-    if (timeElapsedSeconds >= TIME_TO_REMOVE_SECONDS) {
+    let timeElapsedMinutes = dateUtil.dateDiffMinutes(member.dateJoined);
+    if (timeElapsedMinutes >= TIME_TO_REMOVE_MINUTES) {
       let currPoolRef = poolCollectionRef.doc(memberSnapshot.id);
       batch.delete(currPoolRef);
     }
 
     return member;
   });
-  return batch.commit();
+  batch.commit();
+  return true;
 };
 
 module.exports = cleanMatchPool;
