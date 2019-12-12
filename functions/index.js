2d2d2391605264c3ae3de862fe12fa05a47bad94
AllanJeremy <aj.dev254@gmail.com>
2019-12-12 18:48:43 +0300

    [Enhancement] Split scheduled jobs into 3 There was no need to try removing expired chats every 5 minutes. Overkill. Doing it every hour now
    - Clean match pool
    - Remove expired chats
    - Update user nicknames

--- functions/index.js
+++ functions/index.js
@@ -10,17 +10,23 @@ const DEFAULT_TIME_ZONE = "Africa/Nairobi";
 
 //* SCHEDULED FUNCTIONS
 
-// Every 5 minutes
+// Every 5 minutes | Clean match pool (remove people that have been in match pool for a while)
 exports.fiveMinuteSchedule = functions.pubsub
   .schedule("every 5 minutes")
   .timeZone(DEFAULT_TIME_ZONE)
   .onRun(context => {
-    console.debug("Wazzappenin");
-    removeExpiredChats();
     cleanMatchPool();
   });
 
-// Daily
+// Every 60 minutes - 1 hour | Remove expired chats
+exports.hourlySchedule = functions.pubsub
+  .schedule("every 60 minutes")
+  .timeZone(DEFAULT_TIME_ZONE)
+  .onRun(context => {
+    removeExpiredChats();
+  });
+
+// Daily | Update user nicknames
 exports.dailySchedule = functions.pubsub
   .schedule("0 0 * * *")
   .timeZone(DEFAULT_TIME_ZONE)
